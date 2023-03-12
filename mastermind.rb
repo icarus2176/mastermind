d7795a8605b1e64ae72154d39a8765d68713490e
icarus2176 <ashe.kemuri@gmail.com>
2023-03-12 15:05:28 -0500

    create initialize methods

--- mastermind.rb
+++ mastermind.rb
@@ -1,17 +1,23 @@
 module Mastermind
-  class Computer
-
-  end
-  
   class Player
+    def initialize(name, role)
+      @name, @role = name, role
+    end
+  end
+
+  class Computer < Player
 
   end
 
   class Board
-
+    def initialize(code)
+      @code = code
+    end
   end
 
   class Game
-
+    def new_game()
+      
+    end
   end 
 end
