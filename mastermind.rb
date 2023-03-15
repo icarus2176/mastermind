a7121edb56dcd70ad96318811b2010a2d1125453
icarus2176 <ashe.kemuri@gmail.com>
2023-03-15 13:14:57 -0500

    fix bugs

--- mastermind.rb
+++ mastermind.rb
@@ -9,15 +9,16 @@ module Mastermind
   end
 
   class Computer < Player
-    colors = ["R", "B", "Y", "G", "P", "O"]
 
     def create_code
-      @code = array.new()
+      colors = ["R", "B", "Y", "G", "P", "O"]
+      @code = Array.new()
 
       4.times do
-        code.push(colors[rand(5)])
+        color = colors.sample
+        @code.push(color)
       end
-      code
+      @code
     end
   end
 
@@ -43,27 +44,28 @@ module Mastermind
     end
 
     private
-    
+
     def display(result)
-      puts "1) #result[0] 2) #result[1] 3) #result[2] 4) #result[3]"
+      puts "1) #{result[0]} 2) #{result[1]} 3) #{result[2]} 4) #{result[3]}"
     end
   end
 
   class Game
     def new_game()
       puts "Do you want to make the code (1) or guess the code (2)? Type the number then press enter."
-      player_role = gets.chomp
+      @player_role = gets.chomp.to_i
       turn_count = 1
       @player = Player.new()
       @computer = Computer.new()
 
-      if player_role == 1
+      if @player_role == 1
 
-      elsif player_role = 2
+      elsif @player_role == 2
         @board = Board.new(@computer.create_code)
 
         until(turn_count > 12)
-          result = check_answer(@player.guess)
+          puts "reaches until loop"
+          result = @board.check_answer(@player.guess)
           if result.all?("C")
             win
           end
@@ -73,9 +75,9 @@ module Mastermind
     end
 
     def win
-      if player_role == 1
+      if @player_role == 1
       puts "Computer couldn't guess the code. You win!"
-      elsif player_role == 2
+      elsif @player_role == 2
         puts "You guessed the code! You win!"
       end
 
@@ -83,9 +85,9 @@ module Mastermind
     end
 
     def lose
-      if player_role == 1
+      if @player_role == 1
         puts "Computer guessed the code. You lose."
-      elseif player_role == 2
+      elseif @player_role == 2
         puts "You ran out of guesses. You lose."
       end
 
@@ -97,7 +99,11 @@ module Mastermind
     puts "Do you want to play again? Y/N"
     answer = gets.chomp
 
-    if answer = "Y"
+    if answer == "Y"
       new_game
     end 
+  end
+  game = Game.new
+  game.new_game
 end
+
