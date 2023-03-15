e277a44e8df7a2ad681156edceb4246916f1df68
icarus2176 <ashe.kemuri@gmail.com>
2023-03-15 12:14:28 -0500

    initialize Player  and Computer in Game class method

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,7 @@
 module Mastermind
   class Player
-    def initialize(name, role)
-      @name, @role = name, role
+    def initialize(name, is_guesser)
+      @name, @is_guesser = name, is_guesser
     end
   end
 
@@ -17,7 +17,16 @@ module Mastermind
 
   class Game
     def new_game()
+      puts "Do you want to make the code (1) or guess the code (2)? Type the number then press enter."
+      player_role = gets.chomp
       
+      if player_role == 1
+        player = Player.new("Player", false)
+        computer = Computer.new("Computer", true)
+      elsif player_role = 2
+        player = Player.new("Player", true)
+        computer = Computer.new("Computer", false)
+      end
     end
   end 
 end
